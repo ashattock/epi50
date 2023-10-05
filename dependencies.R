4ea6325271e8cbc02a306ef1c151c891061af0db
Andrew Shattock <andrewjames.shattock@unibas.ch>
2023-10-05 12:34:14 +0200

    Add main launch and dependencies files

---
+++ dependencies.R
@@ -0,0 +1,117 @@
+###########################################################
+# DEPENDENCIES
+#
+# Deal with all package dependencies in one place.
+#
+###########################################################
+
+# ---- R version check ----
+
+# R versions for which this project has been tested and is stable
+stable_versions = "4.3.0"
+
+# R versions for which this project is stable (as a string)
+stable_str = paste(stable_versions, collapse = ", ")
+
+# Get details of R version currently running
+version_info = R.Version()
+
+# Construct version number from list details
+version_num = paste0(version_info$major, ".",  version_info$minor)
+
+# Throw an error if this R version is unsuitable
+if (!version_num %in% stable_versions)
+  stop("This software is stable with R version(s): ", stable_str,
+       " (currently running ", version_num, ")")
+
+# Clear global environment
+rm(list = ls())
+
+# ---- Source files ----
+
+# Scripts that should bot be sourced
+no_src = c("launch.R", "dependencies.R")
+
+# All R files, and those to source
+all_files = list.files(pattern = ".+\\.R$")
+src_files = setdiff(all_files, no_src)
+
+# Source each of these files
+for (file in src_files)
+  source(file)
+
+# ---- Define packages ----
+
+# Complete list of all R packages required for this project
+packages = c("tidyverse",      # Includes ggplot2, dplyr, tidyr (www.tidyverse.org/packages/)
+             "data.table",     # Next generation dataframes
+             "gsubfn",         # Output multiple variables from functions
+             "magrittr",       # Additional pipe operators, such as %<>%
+             "rlist",          # List-related operation functions
+             "wrapr",          # Convenience functions (eg qc)
+             "stats",          # Statistical calculations and random number generation
+             "stats4",         # MLE algorithm
+             "matrixStats",    # Matrix row and column operations
+             "tgp",            # Latin hypercube sampler
+             "sp",             # Geo-spatial functionality
+             "malariaAtlas",   # Geo-spatial functionality
+             "broom",          # Geo-spatial support functions
+             "Hmisc",          # Geo-spatial support functions
+             "countrycode",    # Convert country ISO <-> names
+             "xlsx",           # Data loading functionality (or use readxl?)
+             "mclm",           # Data loading functionality
+             "lubridate",      # Data formatting functionality
+             "naniar",         # Data formatting functionality
+             "gridExtra",      # Plotting functionality
+             "ggpubr",         # Plotting functionality
+             "ggrepel",        # Plotting functionality
+             "scales",         # Plotting functionality
+             "pals",           # Colour palettes
+             "colorspace")     # Colour palettes
+
+# Other packages that are likely needed...
+# DBI
+# DT
+# RSQLite
+# wpp2019
+# rnaturalearthdata
+# bigrquery
+# dbplyr
+# knitr
+# rmarkdown
+# testthat
+
+# ---- Install and/or load R packages with pacman ----
+
+message("* Installing required packages")
+
+# Check whether pacman itself has been installed
+pacman_installed = "pacman" %in% rownames(installed.packages())
+
+# If not, install it
+if (!pacman_installed) 
+  install.packages("pacman")
+
+# Load pacman
+library(pacman) 
+
+# Load all required packages, installing them if required
+pacman::p_load(char = packages)
+
+# ---- Redefine or unmask particular functions ----
+
+# Unmask certain commonly used functions
+select  = dplyr::select
+filter  = dplyr::filter
+rename  = dplyr::rename
+recode  = dplyr::recode
+count   = dplyr::count
+union   = dplyr::union
+predict = stats::predict
+
+# ---- Clean up ----
+
+# Tidy up console after package loading
+if (interactive()) clf()  # Close figures
+if (interactive()) clc()  # Clear console
+
---
+++ launch.R
@@ -0,0 +1,42 @@
+###########################################################
+# LAUNCH
+#
+# Main launch function for WHO EPI50 analysis.
+#
+###########################################################
+
+# Set working directory to sourced file
+if (interactive()) setwd(getSrcDirectory(function() {}))
+
+# Load all required packages and functions
+source("dependencies.R")
+
+message("Running EPI50 pipeline")
+
+# Set options (see options.R)
+o = set_options(do_step = 0)
+
+# Other possible pre-steps: 
+#  - Generate database
+#  - Population projection 
+
+# Additional functionality:
+# prep_sia()
+# explore_coverage()
+# explore_dalys()
+# explore_nonlinear()
+
+# Step 1) Calculate and impute relative risk
+run_relative_risk()  # See relative_risk.R
+
+# Step 2) Impact factors
+run_impact_factors()  # See impact_factors.R
+
+# Step 3) Generate uncertainty draws
+run_uncertainty()  # See uncertainty.R
+
+# Step 4) Produce results
+run_results()  # See results.R
+
+# Finish up
+message("* Finished!")
