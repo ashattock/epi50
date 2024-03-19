ab3b938f00d9d903d726969deffc9820070facd0
Andrew Shattock <andrewjames.shattock@unibas.ch>
2024-03-19 15:35:36 +0100

    Use updated VIMC files which contain DALY estimates

--- directories.R
+++ directories.R
@@ -26,11 +26,14 @@ prepare_dirs = function(o) {
   pth$input  = file.path(pth$code, "input")
   pth$config = file.path(pth$code, "config")
   
-  # Parent path of all external model output files
-  pth$extern = file.path(pth$code, "extern")
-  
   # Path to log files
   pth$log = file.path(pth$code, "log")
+  
+  # Parent path of all VIMC results files
+  pth$vimc = file.path(pth$input, "vimc")
+  
+  # Parent path of all external model output files
+  pth$extern = file.path(pth$code, "extern")
   
   # ---- Output directories ----
   
@@ -62,15 +65,6 @@ prepare_dirs = function(o) {
   
   # Append paths to o list
   o = set_dirs(o, pth)
-  
-  # ---- Data URLs ----
-  
-  # stem = "https://ghdx.healthdata.org/sites/default/files/record-attached-files/"
-  # 
-  # o$data = list(
-  #   sdi = c("IHME_GBD_2019_SDI_1990_2019_Y2020M10D15.CSV", 
-  #           "IHME_GBD_2019_SDI_1970_1989_Y2020M10D15.CSV"), 
-  #   haqi = "IHME_GBD_2019_HAQ_1990_2019_DATA.zip")
     
   return(o)
 }
